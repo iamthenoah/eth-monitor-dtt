34b4250c9a2572cac513faa054a9123296b32fa9
Noah <greffnoah@gmail.com>
2021-08-28 01:28:34 +0200

    revisited sortBy function to be more elegant

--- src/apis/util/Compare.ts
+++ src/apis/util/Compare.ts
@@ -4,10 +4,10 @@
  * Sort specified propery in given object T.
  * @param {String} property Property to compare
  */
-export const sortBy = <T>(property: string) : (a: T, b: T) => number => {
+export const sortBy = <T extends Record<string, unknown>>(property: keyof T) : (a: T, b: T) => number => {
     return (a: T, b: T) => {
-        if ((a as any)[property] < (b as any)[property]) return 1
-        else if ((a as any)[property] > (b as any)[property]) return -1
+        if (a[property] < b[property]) return 1
+        else if (a[property] > b[property]) return -1
         return 0
     }
 }
