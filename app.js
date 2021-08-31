ef8b37a05bb7e502c864a5c13d13dbcaa0099f60
Noah <greffnoah@gmail.com>
2021-08-31 16:54:51 +0200

    fixed file with lint

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
+import express from 'express'
+import path from 'path'
 
-const app = express();
-const port = process.env.PORT || 8080;
+const app = express()
+const port = process.env.PORT || 8080
 
 app.use(express.static(path.resolve(__dirname, 'dist/')))
-app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname, 'dist/index.html')));
-app.listen(port);
+app.get(/.*/, (_req, res) => res.sendFile(path.resolve(__dirname, 'dist/index.html')))
+app.listen(port)
 
-console.log('App Running; PORT:', port);
+console.log('App Running; PORT:', port)
